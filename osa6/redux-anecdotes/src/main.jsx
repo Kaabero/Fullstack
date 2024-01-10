c4786fdca7a17ebadaf976a5703f7e6e95a19958
Katri <katri.kuoppala@helsinki.fi>
2024-01-10 10:46:01 +0200

    6.16 anekdootit ja backend, step3

--- osa6/redux-anecdotes/src/main.jsx
+++ osa6/redux-anecdotes/src/main.jsx
@@ -1,13 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import Store from './store'
+import store from './store'
 import { Provider } from 'react-redux'
 import App from './App'
-
-
-
-const store = Store()
-
-console.log('store.getstage()', store.getState())
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -4,20 +4,14 @@ import reducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
 
-const Store = () => {
 
-    const store = configureStore({
-        reducer: {
+
+const store = configureStore({
+    reducer: {
         anecdotes: reducer,
         filter: filterReducer,
         notification: notificationReducer
-        }
-    })
-    console.log('store.getstage()', store.getState())
-    return store
-}
+    }
+})
 
-  
-
-
-export default Store
+export default store
