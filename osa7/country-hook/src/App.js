c7dddc1d48a92f0d5ae82615a8d3a55f3ed0e18c
Katri <katri.kuoppala@helsinki.fi>
2024-01-17 21:19:52 +0200

    7.7: country hook

---
+++ osa7/country-hook/src/App.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+// `${baseUrl}/${name}`
+
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+
+  const [country, setCountry] = useState(null)
+  const baseUrl = `https://studies.cs.helsinki.fi/restcountries`
+
+
+  
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`${baseUrl}/api/name/${name}`)
+        .then(response => {
+          setCountry(response.data)
+        })
+        .catch((error) => {
+          if (error.response.status === 404) {
+            console.log('error', error.message)
+            setCountry('not found')
+            return country
+          }
+        })
+    }
+  }, )
+
+  if (!country) {
+    return null
+  }
+
+  return country
+}
+
+
+const Country = ({ country }) => {
+  
+
+  if (country === 'not found') {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+  if (!country) {
+    return null
+  }
+  
+  if (country) {
+
+    return (
+      <div>
+        <h3>{country.name.common} </h3>
+        <div>capital {country.capital} </div>
+        <div>population {country.population}</div> 
+        <h1>{country.flag}</h1> 
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+  
+
+  const fetch = (e) => {
+    
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+  
+  return (
+    
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+      
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
