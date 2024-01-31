77123476d148bd524ce359f5402a9313446e5877
Katri <katri.kuoppala@helsinki.fi>
2024-01-31 13:58:11 +0200

    8.7: Kirjailijan syntymävuoden päivitys

--- osa8/library-backend.js
+++ osa8/library-backend.js
@@ -110,6 +110,11 @@ const typeDefs = `
         author: String
         genres: [String]
     ): Book
+
+    editAuthor(
+        name: String
+        setBornTo: Int
+      ): Author
   }
 `
 
@@ -152,7 +157,20 @@ const resolvers = {
         console.log('updatedauthors', authors)
       }
       return book
-    }
+    },
+
+    editAuthor: (root, args) => {
+        const author = authors.find(a => a.name === args.name)
+        if (!author) {
+          return null
+        }
+    
+        const updatedAuthor = { ...author, born: args.setBornTo }
+        console.log('authors 1', authors )
+        authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+        console.log('authors 2', authors)
+        return updatedAuthor
+    } 
   }
 }
 
