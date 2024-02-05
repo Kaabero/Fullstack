1d4bb9a2684eaf8a3e65b4b248b0b7a10b430986
Katri <katri.kuoppala@helsinki.fi>
2024-02-05 09:11:50 +0200

    8.15 Tietokanta, osa 3

--- osa8/library-backend/library-backend.js
+++ osa8/library-backend/library-backend.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
@@ -81,7 +82,7 @@ const resolvers = {
         const booksByAuthor =  books.filter(b => b.author.toString() === author._id.toString())
           
         return booksByAuthor.filter(book => book.genres.find(genre => genre === args.genre))
-        
+
         }
         
         if (args.author) {
@@ -138,24 +139,58 @@ const resolvers = {
   Mutation: {
     addBook: async (root, args) => {
       const authors = await Author.find({})
+      const books = await Book.find({})
       console.log('args', args)
-      
+
 
       if ((authors.find(a => a.name === args.author)) === undefined) {
-        const author = new Author({name: args.author, books: []})
-        console.log('new author id', author._id)
-        const book = new Book({
-          title: args.title,
-          author: author._id,
-          published: args.published,
-          genres: args.genres
-        })
-        const savedBook = await book.save()
-        author.books = author.books.concat(savedBook._id)
-        await author.save()
-        return savedBook
+        try {
+          const author = new Author({name: args.author, books: []})
+          await author.save()
+          console.log('new author id', author._id)
+          const book = new Book({
+            title: args.title,
+            author: author._id,
+            published: args.published,
+            genres: args.genres
+          })
+
+          const savedBook = await book.save()
+          author.books = author.books.concat(savedBook._id)
+          await author.save()
+          return savedBook
         
-        
+        } catch (error) {
+          if (args.author.length < 4 ) {
+            throw new GraphQLError('Invalid author name', {
+              extensions: {
+                code: 'BAD_USER_INPUT',
+                invalidArgs: args.author, 
+                error,
+              }
+            })
+          }
+          if (books.find(b => b.title === args.title)) {
+            throw new GraphQLError('Title must be unique', {
+              extensions: {
+                code: 'BAD_USER_INPUT',
+                invalidArgs: args.title,
+                error
+              }
+            })
+          }
+          
+          if (args.title.length <5 || !args.title) {
+            throw new GraphQLError('Saving book failed', {
+              extensions: {
+                code: 'BAD_USER_INPUT',
+                invalidArgs: args.title, 
+                error,
+              }
+            })
+          }
+        }
+      
       } else {
 
       const author = authors.find(a => a.name === args.author)
@@ -167,10 +202,31 @@ const resolvers = {
         published: args.published,
         genres: args.genres
       })
-      const savedBook = await book.save()
-      author.books = author.books.concat(savedBook._id)
-      await author.save()
-      return savedBook
+      try {
+        const savedBook = await book.save()
+        author.books = author.books.concat(savedBook._id)
+        await author.save()
+        return savedBook
+      } catch (error) {
+        if (books.find(b => b.title === args.title)) {
+          throw new GraphQLError('Title must be unique', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.title,
+              error
+            }
+          })
+        }
+        
+        if (args.title.length <5 || !args.title.lenght) {
+        throw new GraphQLError('Saving book failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.title, 
+            error
+          }
+        })
+      }}
       }
     },
     
